5998996f6c0f27331f6ecb93b64cf6a40e32dbeb
Garry <gmisal2002@gmail.com>
2023-08-18 14:37:13 +0530

    Backend Done view initialized

---
+++ view/src/App.jsx
@@ -0,0 +1,14 @@
+import Heading from "./components/Heading"
+import ShortenerCard from "./components/ShortenerCard"
+
+function App() {
+
+  return (
+    <div className="container mx-auto mt-24">
+      <Heading />
+      <ShortenerCard />
+    </div>
+  )
+}
+
+export default App
---
+++ view/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">Free URL Shortener</h1>
+            <p className="text-gray-600">Shorten your long URLs for free,Create short & memorable links in seconds.</p>
+        </div>
+    )
+}
+
+export default Heading
